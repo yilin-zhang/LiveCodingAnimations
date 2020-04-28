7b0111b5960c2a0b778c31ca7a65346e66a499c7
Yilin Zhang <yzhang0129@outlook.com>
2020-04-27 21:31:04 -0400

    Periodically trigger the animations.

--- sketch.js
+++ sketch.js
@@ -64,16 +64,44 @@ class PString {
 // Instrument Definitions
 class Guitar {
     constructor(width, height, color) {
-        this.strings = new Array(5);
+        this.strings = new Array(6);
         this.length = width;
         this.height = height;
         this.color = color;
+        this.distance = 0;
+
         this.strings[0] = new PString(this.length, this.color, 4.5, 0.4, 0.5, 0.85);
         this.strings[1] = new PString(this.length, this.color, 4, 0.35, 0.4, 0.83);
         this.strings[2] = new PString(this.length, this.color, 3.5, 0.2, 0.3, 0.8);
         this.strings[3] = new PString(this.length, this.color, 3, 0.15, 0.2, 0.77);
         this.strings[4] = new PString(this.length, this.color, 2.5, 0.1, 0.15, 0.75);
         this.strings[5] = new PString(this.length, this.color, 2, 0.05, 0.1, 0.73);
+
+        this.dur = 0;
+        this.durCounter = 0;
+    }
+
+    trigger(distance, dur) {
+        this.distance = distance;
+        this.dur = dur;
+        this.durCounter = dur;
+    }
+
+    reset() {
+        this.dur = 0;
+        this.durCounter = 0;
+        this.distance = 0;
+    }
+
+    updateStatus() {
+        if (this.dur === 0)
+            return;
+
+        if (this.durCounter === 0){
+            this.durCounter = this.dur;
+            this.pull(this.distance);
+        }
+        this.durCounter--;
     }
 
     pull(distance) {
@@ -84,6 +112,7 @@ class Guitar {
 
     draw(x, y) {
         rectMode(CORNER);
+        this.updateStatus();
         for (let i=0; i < this.strings.length; i++) {
             this.strings[i].update();
         }
@@ -104,6 +133,33 @@ class Bass {
         this.strings[1] = new PString(this.length, this.color, 5, 0.35, 0.4, 0.83);
         this.strings[2] = new PString(this.length, this.color, 4.5, 0.2, 0.3, 0.8);
         this.strings[3] = new PString(this.length, this.color, 4, 0.15, 0.2, 0.77);
+
+        this.dur = 0;
+        this.durCounter = 0;
+    }
+
+
+    trigger(distance, dur) {
+        this.distance = distance;
+        this.dur = dur;
+        this.durCounter = dur;
+    }
+
+    reset() {
+        this.dur = 0;
+        this.durCounter = 0;
+        this.distance = 0;
+    }
+
+    updateStatus() {
+        if (this.dur === 0)
+            return;
+
+        if (this.durCounter === 0){
+            this.durCounter = this.dur;
+            this.pull(this.distance);
+        }
+        this.durCounter--;
     }
 
     pull(distance) {
@@ -114,6 +170,7 @@ class Bass {
 
     draw(x, y) {
         rectMode(CORNER);
+        this.updateStatus();
         for (let i=0; i < this.strings.length; i++) {
             this.strings[i].update();
         }
@@ -186,19 +243,60 @@ class Keyboard {
         this.whiteWidth = this.width / 7;
         this.blackWidth = this.whiteWidth * 0.7;
         this.blackHeight = this.height * 0.7;
+        this.triggeredSustain = 10;
+
         this.status = new Array(12);
         for (let i=0; i<12; i++) {
             this.status[i] = false;
         }
+
         this.durCounter = new Array(12);
+        for (let i=0; i<12; i++) {
+            this.durCounter[i] = 0;
+        }
+
+        this.dur = new Array(12);
         for (let i=0; i<12; i++) {
             this.durCounter[i] = 0;
         }
     }
 
-    trigger(keyNum) {
+    trigger(keyNum, dur) {
         this.status[keyNum] = true;
-        this.durCounter[keyNum] = 20;
+        this.dur[keyNum] = dur; // the period
+        this.durCounter[keyNum] = dur; // the period
+    }
+
+    relase(keyNum) {
+        this.status[keyNum] = false;
+    }
+
+    reset() {
+        for (let i=0; i<12; i++) {
+            this.status[i] = false;
+        }
+
+        for (let i=0; i<12; i++) {
+            this.durCounter[i] = 0;
+        }
+
+        for (let i=0; i<12; i++) {
+            this.dur[i] = 0;
+        }
+    }
+
+    updateStatus(keyNum) {
+        if (this.dur[keyNum] <= this.triggeredSustain || this.dur[keyNum] === 0)
+            return;
+
+        if (this.dur[keyNum] - this.durCounter[keyNum] >= this.triggeredSustain) {
+            this.status[keyNum] = false;
+            if (this.durCounter[keyNum] === 0){
+                this.status[keyNum] = true;
+                this.durCounter[keyNum] = this.dur[keyNum];
+            }
+        }
+        this.durCounter[keyNum]--;
     }
 
     draw(x, y) {
@@ -210,15 +308,14 @@ class Keyboard {
         let num;
         for (let i=0; i<whiteKeyNum.length; i++){
             num = whiteKeyNum[i];
+            // status
             if (this.status[num]){
                 fill(this.triggeredkeycolor);
-                this.durCounter[num]--;
-                if (this.durCounter[num] === 0){
-                    this.status[num] = false;
-                }
             } else {
                 fill(this.whitekeycolor);
             }
+            this.updateStatus(num);
+            // draw
             if (num <= 4) {
                 rect(x+num/2*this.whiteWidth, y, this.whiteWidth, this.height);
             } else {
@@ -227,15 +324,14 @@ class Keyboard {
         }
         for (let i=0; i<blackKeyNum.length; i++){
             num = blackKeyNum[i];
+            // status
             if (this.status[num]){ // 1, 3, 6, 8, 10
                 fill(this.triggeredkeycolor);
-                this.durCounter[num]--;
-                if (this.durCounter[num] === 0){
-                    this.status[num] = false;
-                }
             } else {
                 fill(this.blackkeycolor);
             }
+            this.updateStatus(num);
+            // draw
             if (num <= 3) {
                 rect(x+(num+1)/2*this.whiteWidth-this.blackWidth/2, y, this.blackWidth, this.blackHeight);
             } else {
@@ -254,6 +350,10 @@ let waveform;
 let guitar;
 let bass;
 let keyboard;
+
+let guitar_dur = 0;
+let bass_dur = 0;
+let keyboard_dur = 0;
 
 function setup() {
     c = createCanvas(windowWidth, windowHeight);
@@ -317,55 +417,59 @@ socket.on('message', function(obj) {
     let status = document.getElementById("status");
     status.innerHTML = obj[0];
 
-    let o = JSON.parse(obj[1])
-    let attributes = o.attributes
-    let player = o.type;
+    let info = JSON.parse(obj[1])
+    console.log(info);
+    let attributes = info.attributes;
+    let player = info.type;
 
-    if (player == 'pluck')
+    if (player === 'pluck')
+    {
+        guitar.reset();
+        attributes.forEach((a) => {
+            if (a.hasOwnProperty('amp'))
+            {
+                let amp = a.amp;
+                if (Array.isArray(amp)) {
+                    const total = amp.reduce((acc, c) => acc + c, 0);
+                    guitar.trigger(total / amp.length * 80, 20);
+                }
+                else
+                    guitar.pull(amp * 80, 20);
+            }
+        });
+    }
+
+    if (player === 'bass')
     {
         attributes.forEach((a) => {
             if (a.hasOwnProperty('amp'))
             {
                 let amp = a.amp;
                 if (Array.isArray(amp)) {
-                    amp.forEach((p) => {
-                        guitar.pull(p * 80);
-                    });
+                    const total = amp.reduce((acc, c) => acc + c, 0);
+                    bass.trigger(total / amp.length * 80, 20);
                 }
                 else
-                    guitar.pull(amp * 80);
+                    bass.trigger(amp * 80, 20);
             }
         });
     }
 
-    if (player == 'bass')
-    {
+    if (player === 'pads'){
+        keyboard.reset();
         attributes.forEach((a) => {
-            if (a.hasOwnProperty('amp'))
+            if (a.hasOwnProperty('degree'))
             {
-                let amp = a.amp;
-                if (Array.isArray(amp)) {
-                    amp.forEach((p) => {
-                        bass.pull(p * 80);
+                let pitches = a.degree;
+                if (Array.isArray(pitches)) {
+                    pitches.forEach((p) => {
+                        keyboard.trigger(p, 20);
                     });
                 }
-                else
-                    bass.pull(amp * 80);
+                else {
+                    keyboard.trigger(pitches, 20);
+                }
             }
         });
     }
-
-    attributes.forEach((a) => {
-        if (a.hasOwnProperty('degree'))
-        {
-            let pitches = a.degree;
-            if (Array.isArray(pitches)) {
-                pitches.forEach((p) => {
-                    keyboard.trigger(p);
-                });
-            }
-            else
-                keyboard.trigger(pitches)
-        }
-    });
 });
